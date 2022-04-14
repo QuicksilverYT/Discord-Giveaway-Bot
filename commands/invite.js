3059d53bd49ca93a4030c2b1e4d70bf4569c319e
Quicksilver_YT <96615931+QuicksilverYT@users.noreply.github.com>
2022-04-14 17:01:06 +0530

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+
+module.exports.run = async (client, message, args) => {
+    let prefix = config.prefix;
+    if (message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+
+    let invite = new Discord.MessageEmbed()
+    .setTitle("Invite & Support Link!")
+    .addField("Support Server", "[Click to join support Server](https://discord.gg/HAsJKecZCa)")
+    .setTimestamp()
+    .setFooter(`Requested by ${message.author.tag}`, client.user.displayAvatarURL())
+    message.channel.send(invite);
+}
+
+module.exports.help = {
+    name: "invite"
+}
